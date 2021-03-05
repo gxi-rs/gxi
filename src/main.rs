36e9599d55eda0014434ec05083cbac9a17e5002
aniketfuryrocks <prajapati.ani306@gmail.com>
2021-03-05 18:50:25 +0530

    mv test to main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,25 @@
-mod comp;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+use crate::nodes::container::Container;
+use crate::nodes::node::Node;
+use crate::nodes::views::grid::Grid;
+use crate::nodes::widgets::button::Button;
+
 mod nodes;
 
-fn main() {}
+fn main() {
+    let mut container: Rc<RefCell<Box<dyn Container>>> =
+        Rc::new(RefCell::new(Box::new(Grid::default())));
+    {
+        container
+            .borrow_mut()
+            .get_child_mut()
+            .get_or_insert_with(|| {
+                Node::Widget(Box::new(Button {
+                    parent: container.clone(),
+                }))
+            });
+    }
+}
+
