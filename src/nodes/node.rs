1ebacb1e5a7681ca1d5228dbfb9c1b967af215de
aniketfuryrocks <prajapati.ani306@gmail.com>
2021-03-05 18:52:45 +0530

    macro export

--- src/nodes/node.rs
+++ src/nodes/node.rs
@@ -12,3 +12,24 @@ pub trait NodeTrait {
     fn get_child_mut(&mut self) -> &mut Option<Node>;
     fn get_sibling_mut(&mut self) -> &mut Option<Node>;
 }
+
+#[macro_export]
+macro_rules! impl_node_trait {
+    () => {
+        fn get_child(&self) -> &Option<Node> {
+            &self.child
+        }
+
+        fn get_sibling(&self) -> &Option<Node> {
+            &self.sibling
+        }
+
+        fn get_child_mut(&mut self) -> &mut Option<Node> {
+            &mut self.child
+        }
+
+        fn get_sibling_mut(&mut self) -> &mut Option<Node> {
+            &mut self.sibling
+        }
+    };
+}
