9d9b2476208e093371206f322bb86114d803c1ab
aniketfuryrocks <prajapati.ani306@gmail.com>
2021-03-19 16:58:26 +0530

    foo

--- src/nodes/node.rs
+++ src/nodes/node.rs
@@ -5,10 +5,18 @@ use std::rc::Rc;
 pub type AsyncNode = Rc<RefCell<Box<dyn Node>>>;
 
 pub trait Node {
-    fn get_child(&self) -> &Option<AsyncNode> { unimplemented!() }
-    fn get_child_mut(&mut self) -> &mut Option<AsyncNode> { unimplemented!() }
-    fn get_sibling(&self) -> &Option<AsyncNode> { unimplemented!() }
-    fn get_sibling_mut(&mut self) -> &mut Option<AsyncNode> { unimplemented!() }
+    fn get_child(&self) -> &Option<AsyncNode> {
+        unimplemented!()
+    }
+    fn get_child_mut(&mut self) -> &mut Option<AsyncNode> {
+        unimplemented!()
+    }
+    fn get_sibling(&self) -> &Option<AsyncNode> {
+        unimplemented!()
+    }
+    fn get_sibling_mut(&mut self) -> &mut Option<AsyncNode> {
+        unimplemented!()
+    }
     fn init_child(
         &mut self, _f: Box<dyn Fn() -> AsyncNode>, _add_widget: bool,
     ) -> (AsyncNode, bool) {
--- src/nodes/node_macros.rs
+++ src/nodes/node_macros.rs
@@ -1,9 +1,13 @@
 #[macro_export]
 macro_rules! impl_node_trait {
     () => {
-        fn as_any(&self) -> &dyn Any { self }
+        fn as_any(&self) -> &dyn Any {
+            self
+        }
 
-        fn as_any_mut(&mut self) -> &mut dyn Any { self }
+        fn as_any_mut(&mut self) -> &mut dyn Any {
+            self
+        }
     };
 }
 
@@ -42,9 +46,13 @@ macro_rules! impl_node_trait_init_sibling {
                 _ => (self.sibling.as_ref().unwrap().clone(), false),
             }
         }
-        fn get_sibling(&self) -> &Option<AsyncNode> { &self.sibling }
+        fn get_sibling(&self) -> &Option<AsyncNode> {
+            &self.sibling
+        }
 
-        fn get_sibling_mut(&mut self) -> &mut Option<AsyncNode> { &mut self.sibling }
+        fn get_sibling_mut(&mut self) -> &mut Option<AsyncNode> {
+            &mut self.sibling
+        }
     };
 }
 
@@ -73,8 +81,12 @@ macro_rules! impl_node_trait_init_child {
 #[macro_export]
 macro_rules! impl_node_trait_get_child {
     () => {
-        fn get_child(&self) -> &Option<AsyncNode> { &self.child }
+        fn get_child(&self) -> &Option<AsyncNode> {
+            &self.child
+        }
 
-        fn get_child_mut(&mut self) -> &mut Option<AsyncNode> { &mut self.child }
+        fn get_child_mut(&mut self) -> &mut Option<AsyncNode> {
+            &mut self.child
+        }
     };
 }
