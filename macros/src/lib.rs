2bb2d6cbdb8bed18620665b4745842e634556019
aniketfuryrocks <prajapati.ani306@gmail.com>
2021-03-23 16:09:26 +0530

    call functions as in brackets

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,33 +1,65 @@
 use proc_macro::TokenStream;
 
-use quote::quote;
-use syn::{Block, Error, Ident};
-use syn::parse::{Parse, ParseBuffer, Parser, ParseStream};
+use quote::{quote, ToTokens};
+use syn::{Block, Error, Expr, Ident, Stmt};
+use syn::__private::TokenStream2;
+use syn::parse::{Parse, ParseStream};
 
 struct Combinations {
     name: Ident,
-    block: Block,
+    exprs: Vec<TokenStream2>,
 }
 
 impl Parse for Combinations {
     fn parse(input: ParseStream) -> Result<Self, Error> {
         let name = input.parse()?;
-        let block = input.parse()?;
-        Ok(Combinations { name, block })
+        let mut exprs = vec![];
+        {
+            let block: Block = input.parse()?;
+            for x in block.stmts {
+                println!("{}", x.to_token_stream().to_string());
+                match x {
+                    Stmt::Semi(s, _) => {
+                        match s {
+                            Expr::Assign(e) => {
+                                let left = e.left;
+                                let right = e.right;
+                                exprs.push(format!("node.widget.set_{}({});",
+                                                   left.to_token_stream().to_string(),
+                                                   right.to_token_stream().to_string())
+                                    .parse::<TokenStream2>().unwrap());
+                            }
+                            _ => panic!("expected an Assignment Expression")
+                        }
+                    }
+                    _ => { panic!("expected an Expression") }
+                }
+            }
+        }
+        Ok(Combinations { name, exprs })
     }
 }
 
 #[proc_macro]
 pub fn proc_node(item: TokenStream) -> TokenStream {
-    let Combinations { name, .. } = syn::parse_macro_input!(item as Combinations);
-    let result = quote! {
+    let Combinations { name, exprs } = syn::parse_macro_input!(item as Combinations);
+
+    (quote! {
         let node = {
-            let mut node_borrow = node.as_ref().borrow_mut();
-            let node = Rc::clone(&node);
-            node_borrow.init_child(Box::new(move || #name::new(node.clone())), true).0
+            let (node, is_new) = {
+                let mut node_borrow = node.as_ref().borrow_mut();
+                let node = Rc::clone(&node);
+                node_borrow.init_child(Box::new(move || #name::new(node.clone())), true)
+            };
+            {
+                let mut node_borrow = node.as_ref().borrow_mut();
+                let node = node_borrow.as_any_mut().downcast_mut::<#name>().unwrap();
+                let state = state.as_ref().borrow();
+                #(#exprs)*
+            }
+            node
         };
-    };
-    result.into()
+    }).into()
 }
 /*
 #[proc_macro]
