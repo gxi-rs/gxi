e97e018ebc05739015eb7d5bba8db0c311383759
aniketfuryrocks <prajapati.ani306@gmail.com>
2021-03-23 17:30:09 +0530

    rm comment

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -81,29 +81,3 @@ pub fn proc_node(item: TokenStream) -> TokenStream {
         };
     }).into()
 }
-/*
-#[proc_macro]
-pub fn proc_node(item: TokenStream) -> TokenStream {
-    let name= syn::parse_macro_input!(item as syn::Ident);
-    let name = &name;
-    let result = quote! {
-        {
-            let (node, _is_new) = {
-                let mut node_borrow = $prev_node.as_ref().borrow_mut();
-                let container = Rc::clone(&$prev_node);
-                node_borrow.$type(Box::new(move || $Node::new(container.clone())), true)
-            };
-            {
-                let mut node_borrow = node.as_ref().borrow_mut();
-                let node = node_borrow.as_any_mut().downcast_mut::<$Node>().unwrap();
-                let state = $state.as_ref().borrow();
-                $(
-                    node.widget.$f(state.$v.to_string().as_str());
-                )*
-            }
-            node
-        }
-    };
-    result.into()
-}
-*/
