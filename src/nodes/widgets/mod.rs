7d3703c25dff04326558b47a43fe9a0c9c102cf0
aniketfuryrocks <prajapati.ani306@gmail.com>
2021-03-25 09:45:17 +0530

    panic msg format

--- src/nodes/widgets/mod.rs
+++ src/nodes/widgets/mod.rs
@@ -13,34 +13,34 @@ const PANIC_MSG: &str = "You can't call any function on (). () can only be used 
 
 impl Node for () {
     fn init_child(&mut self, _f: Box<dyn Fn() -> AsyncNode>, _add_widget: bool) -> (AsyncNode, bool) {
-        panic!(PANIC_MSG)
+        panic!("{}", PANIC_MSG);
     }
 
     fn init_sibling(&mut self, _f: Box<dyn Fn() -> AsyncNode>, _add_widget: bool) -> (AsyncNode, bool) {
-        panic!(PANIC_MSG)
+        panic!("{}", PANIC_MSG);
     }
 
     fn as_any(&self) -> &dyn Any {
-        panic!(PANIC_MSG)
+        panic!("{}", PANIC_MSG);
     }
 
     fn as_any_mut(&mut self) -> &mut dyn Any {
-        panic!(PANIC_MSG)
+        panic!("{}", PANIC_MSG);
     }
 
     fn get_widget(&self) -> Widget {
-        panic!(PANIC_MSG)
+        panic!("{}", PANIC_MSG);
     }
 
     fn get_widget_as_container(&self) -> Container {
-        panic!(PANIC_MSG)
+        panic!("{}", PANIC_MSG);
     }
 
     fn get_parent(&self) -> AsyncNode {
-        panic!(PANIC_MSG)
+        panic!("{}", PANIC_MSG);
     }
 
     fn new(_parent: AsyncNode) -> AsyncNode {
-        panic!(PANIC_MSG)
+        panic!("{}", PANIC_MSG);
     }
 }
