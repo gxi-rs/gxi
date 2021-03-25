24b34d6832f0f6641aecb941bb61fe0c5914a579
aniketfuryrocks <prajapati.ani306@gmail.com>
2021-03-25 09:40:38 +0530

    panic msg constant

--- src/nodes/widgets/mod.rs
+++ src/nodes/widgets/mod.rs
@@ -1,43 +1,48 @@
-pub use button::*;
-use crate::{AsyncNode, Node};
+use std::any::Any;
 use std::cell::RefCell;
 use std::rc::Rc;
+
 use gtk::{Container, Widget};
-use std::any::Any;
+
+pub use button::*;
+
+use crate::{AsyncNode, Node};
 
 pub mod button;
 mod empty;
 
+const PANIC_MSG: &str = "You can't call any function on (). () can only be used as an empty Node without any child or sibling";
+
 impl Node for () {
     fn init_child(&mut self, _f: Box<dyn Fn() -> AsyncNode>, _add_widget: bool) -> (AsyncNode, bool) {
-        unimplemented!()
+        panic!(PANIC_MSG)
     }
 
     fn init_sibling(&mut self, _f: Box<dyn Fn() -> AsyncNode>, _add_widget: bool) -> (AsyncNode, bool) {
-        unimplemented!()
+        panic!(PANIC_MSG)
     }
 
     fn as_any(&self) -> &dyn Any {
-        unimplemented!()
+        panic!(PANIC_MSG)
     }
 
     fn as_any_mut(&mut self) -> &mut dyn Any {
-        unimplemented!()
+        panic!(PANIC_MSG)
     }
 
     fn get_widget(&self) -> Widget {
-        unimplemented!()
+        panic!(PANIC_MSG)
     }
 
     fn get_widget_as_container(&self) -> Container {
-        unimplemented!()
+        panic!(PANIC_MSG)
     }
 
     fn get_parent(&self) -> AsyncNode {
-        unimplemented!()
+        panic!(PANIC_MSG)
     }
 
     fn new(parent: AsyncNode) -> AsyncNode {
-        unimplemented!();
+        panic!(PANIC_MSG)
     }
 }
