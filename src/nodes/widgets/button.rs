56e9ff34705ff0451a9a950d997d698cb0c50033
aniketfuryrocks <prajapati.ani306@gmail.com>
2021-03-28 11:53:55 +0530

    drop trait for button

--- src/nodes/widgets/button.rs
+++ src/nodes/widgets/button.rs
@@ -32,3 +32,10 @@ impl Node for Button {
         })))
     }
 }
+
+impl Drop for Button {
+    fn drop(&mut self) {
+        let parent_borrow =  self.parent.as_ref().borrow_mut();
+        parent_borrow.get_widget_as_container().remove(&self.widget);
+    }
+}
