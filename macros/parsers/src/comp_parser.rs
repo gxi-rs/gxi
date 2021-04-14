e765d623228eb912201a60d0c0df9f65cbbd8cc0
aniketfuryrocks <prajapati.ani306@gmail.com>
2021-04-14 17:20:42 +0530

    compiling to both native and web

--- macros/parsers/src/comp_parser.rs
+++ macros/parsers/src/comp_parser.rs
@@ -10,59 +10,28 @@ pub struct CompParser {
 }
 
 #[macro_export]
-macro_rules! comp_init {
-    ($name:ident $state_name:ident { { $($desktop_field:tt)* } { $($desktop_value:tt)* } { $($desktop_channel_it:tt)* } { $($desktop_channel_attach:tt)* } } { $($p:ident : $t:ty = $v:expr)* } { $($render:tt)* } )=> {
-        use std::any::Any;
-        use std::borrow::Borrow;
-        use std::cell::RefCell;
-        use std::rc::Rc;
-        use std::sync::{Mutex, Arc};
+macro_rules! comp_new {
+    ($state_name:ident $parent:ident { $($sender:tt)* } { $($p:ident : $t:ty = $v:expr)* }) => {
+        Rc::new(RefCell::new(Box::new(Self {
+            state: Arc::new(Mutex::new($state_name {
+                $($p:$v),*
+            })),
+            $($sender)*
+            self_substitute : None,
+            $parent,
+            dirty: true,
+            child: None,
+            sibling: None,
+        })));
+    };
+}
 
-        type AsyncState = Arc<Mutex<$state_name>>;
-
-        pub struct $name {
-            pub state: AsyncState,
-            $($desktop_field)*
-            pub parent: WeakNodeRc,
-            pub self_substitute : Option<WeakNodeRc>,
-            pub dirty: bool,
-            pub child: Option<NodeRc>,
-            pub sibling: Option<NodeRc>
-        }
-
+#[macro_export]
+macro_rules! comp_state {
+    ($state_name:ident { $($p:ident : $t:ty = $v:expr)* }) => {
         pub struct $state_name {
             $(pub $p:$t),*
         }
-
-        impl Node for $name {
-            impl_node_for_component!();
-
-            fn new(parent: WeakNodeRc) -> NodeRc {
-                $($desktop_channel_it)*
-                let this:NodeRc = Rc::new(RefCell::new(Box::new(Self {
-                    state: Arc::new(Mutex::new($state_name {
-                        $($p:$v),*
-                    })),
-                    $($desktop_value)*
-                    self_substitute : None,
-                    parent,
-                    dirty: true,
-                    child: None,
-                    sibling: None,
-                })));
-                {
-                    let mut this_borrow = this.as_ref().borrow_mut();
-                    let this_borrow = this_borrow.as_any_mut().downcast_mut::<Self>().unwrap();
-                    this_borrow.self_substitute = Some(Rc::downgrade(&this));
-                }
-                $($desktop_channel_attach)*
-                this
-            }
-
-            $($render)*
-        }
-
-        impl_drop_for_component!($name);
     };
 }
 
@@ -71,31 +40,27 @@ impl Parse for CompParser {
         let name = input.parse::<syn::Ident>()?;
         let state_name = syn::Ident::new(&format!("{}State", quote! {#name}), Span::call_site());
         let state_block = input.parse::<syn::Block>()?;
-        let mut render_func = quote!(
-            fn render(_this: NodeRc) {}
-        );
+        let mut render_func_inner = TokenStream2::new();
         for _ in 0..2 {
             if let Ok(s) = input.parse::<syn::Ident>() {
                 match &s.to_string()[..] {
                     "render" => {
                         let block_content = group::parse_braces(&input)?.content;
                         let content = TreeParser::parse(&block_content)?.tree;
-                        render_func = quote!(
-                            fn render(this: NodeRc) {
-                                let cont = Rc::clone(&this);
-                                let node = cont.clone();
-                                let state = {
-                                    let mut node_borrow = this.as_ref().borrow_mut();
-                                    let node = node_borrow.as_any_mut().downcast_mut::<Self>().unwrap();
-                                    if !node.is_dirty() {
-                                        return;
-                                    }
-                                    node.mark_clean();
-                                    node.state.clone()
-                                };
-                                let state = state.lock().unwrap();
-                                #content
-                            }
+                        render_func_inner = quote!(
+                            let cont = Rc::clone(&this);
+                            let node = cont.clone();
+                            let state = {
+                                let mut node_borrow = this.as_ref().borrow_mut();
+                                let node = node_borrow.as_any_mut().downcast_mut::<Self>().unwrap();
+                                if !node.is_dirty() {
+                                    return;
+                                }
+                                node.mark_clean();
+                                node.state.clone()
+                            };
+                            let state = state.lock().unwrap();
+                            #content
                         );
                     }
                     _ => panic!("Didn't expect this attribute here"),
@@ -104,11 +69,11 @@ impl Parse for CompParser {
         }
 
         #[cfg(feature = "desktop")]
-            let win_block = quote! {{
-                { pub channel_sender: Sender<()>, }
-                { channel_sender, }
-                { let (channel_sender, re) = glib::MainContext::channel(glib::PRIORITY_DEFAULT); }
-                {/*{
+            let (desktop_channel_new, sender_field, sender_struct_field, channel_attach) = (
+            quote! { let (channel_sender, re) = glib::MainContext::channel(glib::PRIORITY_DEFAULT); },
+            quote! { channel_sender, },
+            quote! { pub channel_sender: glib::Sender<()>, },
+            quote! {{
                     let this = this.clone();
                     re.attach(None, move |_| {
                         let this = Rc::clone(&this);
@@ -120,17 +85,61 @@ impl Parse for CompParser {
                         Self::render(this);
                         glib::Continue(true)
                     });
-                }*/}
-            }};
+                }}
+        );
 
         #[cfg(feature = "web")]
-            let win_block = quote! {{{ }{ }{ }{ }}};
+            let (desktop_channel_new, sender_field, sender_struct_field, channel_attach) = (
+            TokenStream2::new(),
+            TokenStream2::new(),
+            TokenStream2::new(),
+            TokenStream2::new(),
+        );
 
         Ok(CompParser {
-            tree: quote!(
-                use glib::Sender;
-                comp_init!(#name #state_name #win_block #state_block {#render_func});
-            ),
+            tree: quote! {
+                use std::any::Any;
+                use std::borrow::Borrow;
+                use std::cell::RefCell;
+                use std::rc::Rc;
+                use std::sync::{Mutex, Arc};
+
+                type AsyncState = Arc<Mutex<#state_name>>;
+
+                comp_state!(#state_name #state_block);
+
+                pub struct #name {
+                    pub state: AsyncState,
+                    #sender_struct_field
+                    pub parent: WeakNodeRc,
+                    pub self_substitute : Option<WeakNodeRc>,
+                    pub dirty: bool,
+                    pub child: Option<NodeRc>,
+                    pub sibling: Option<NodeRc>
+                }
+
+                impl Node for #name {
+                    impl_node_for_component!();
+
+                    fn new(parent: WeakNodeRc) -> NodeRc {
+                        #desktop_channel_new
+                        let this:NodeRc = comp_new!(#state_name parent { #sender_field } #state_block );
+                        {
+                            let mut this_borrow = this.as_ref().borrow_mut();
+                            let this_borrow = this_borrow.as_any_mut().downcast_mut::<Self>().unwrap();
+                            this_borrow.self_substitute = Some(Rc::downgrade(&this));
+                        }
+                        #channel_attach
+                        this
+                    }
+
+                    fn render(this: NodeRc) {
+                        #render_func_inner
+                    }
+                }
+
+                impl_drop_for_component!(#name);
+            }
         })
     }
 }
