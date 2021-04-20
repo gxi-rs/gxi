8af24dca8557ed2db39957a2a7cc1e716a839b25
aniketfuryrocks <prajapati.ani306@gmail.com>
2021-04-20 15:03:21 +0530

    Img

--- binds/web_binds/src/widgets/img.rs
+++ binds/web_binds/src/widgets/img.rs
@@ -7,6 +7,14 @@ impl_web_widget!(Img "img");
 impl Img {
     generate_pub_attr!(alt);
     generate_pub_attr!(crossorigin);
-    generate_pub_attr!(http_equiv);
-    generate_pub_attr!(name);
+    generate_pub_attr!(decoding);
+    generate_pub_attr!(height u32);
+    generate_pub_attr!(ismap bool);
+    generate_pub_attr!(loading);
+    generate_pub_attr!(referrerpolicy);
+    generate_pub_attr!(sizes);
+    generate_pub_attr!(src);
+    generate_pub_attr!(srcset);
+    generate_pub_attr!(width u32);
+    generate_pub_attr!(usemap);
 }
