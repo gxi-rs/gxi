4f5472c1b9e8dab94be55470249cda1afe3dd3f6
aniketfuryrocks <prajapati.ani306@gmail.com>
2021-05-24 18:33:21 +0530

    test

--- tests/src/macros/gxi_macro/conditional.rs
+++ tests/src/macros/gxi_macro/conditional.rs
@@ -23,11 +23,37 @@ gxi! {
                 }
                 {/* Last Comment */}*/
             }
-        ]
+        ],
+        Comp
     }
 }
 
 #[test]
 fn traverse_conditional() {
-    println!("traversing")
+    let root = Root::new_root();
+    let app = {
+        let root_weak = Rc::downgrade(&root);
+        root.borrow_mut().init_child(Box::new(|| App::new(root_weak))).0
+    };
+    //render
+    App::render(app.clone());
+    //start traversing app
+    {
+        let node = check_child_type::<Comp>(app, "Comp");
+        check_sibling_type::<Comp>(node, "Comp");
+    }
+}
+
+fn check_child_type<T: 'static + Node>(node: NodeRc, name: &str) -> NodeRc {
+    let node_borrow = node.as_ref().borrow();
+    let child = node_borrow.get_child().as_ref().unwrap();
+    child.as_ref().borrow().as_any().downcast_ref::<T>().expect(&format!("expected '{}' here", name));
+    child.clone()
+}
+
+fn check_sibling_type<T: 'static + Node>(node: NodeRc, name: &str) -> NodeRc {
+    let node_borrow = node.as_ref().borrow();
+    let child = node_borrow.get_sibling().as_ref().unwrap();
+    child.as_ref().borrow().as_any().downcast_ref::<T>().expect(&format!("expected '{}' here", name));
+    child.clone()
 }
