7862e6fa3cf6edfb9e6ba43e69e0c9119ea4e6d4
Aniket Prajapati <prajapati.ani306@gmail.com>
2021-06-08 11:16:48 +0530

    compiling

--- src/interface/init_member.rs
+++ src/interface/init_member.rs
@@ -1,7 +1,7 @@
-use crate::{InitType, WeakNodeType, StrongNodeType, GxiNodeType};
-use std::ops::Deref;
+use crate::{GxiNodeType, InitType, StrongNodeType, WeakNodeType};
+use std::borrow::{Borrow};
+use std::ops::{Deref,DerefMut};
 use std::rc::Rc;
-use std::borrow::{BorrowMut, Borrow};
 
 // TODO: replace init_type with f32 index
 /// if init_type doesn't already exist then run init() and return clone of the new member
@@ -11,34 +11,27 @@ use std::borrow::{BorrowMut, Borrow};
 pub fn init_member<F: FnOnce(WeakNodeType) -> StrongNodeType>(
     this: StrongNodeType, init_type: InitType, init: F,
 ) -> (StrongNodeType, bool) {
-    let this_borrow = this.as_ref().borrow();
+
+    let mut this_borrow_mut = this.as_ref().borrow_mut();
+    // check if this is a widget
+    if let GxiNodeType::Widget(_) = this_borrow_mut.deref() {
+        panic!("can't add node to widget");
+    }
+    // add accordingly
     match init_type {
         InitType::Child => {
             // check if child already exists
-            match this_borrow.deref() {
-                GxiNodeType::Component(this) => if let Some(child) = this.get_child().deref() {
-                    return (child.clone(), false);
-                }
-                GxiNodeType::Container(this) => if let Some(child) = this.get_child().deref() {
-                    return (child.clone(), false);
-                }
-                _ => panic!("Can't add a node into a widget")
+            if let Some(child) = this_borrow_mut.as_container().unwrap().get_child() {
+                return (child.clone(), true);
             }
             // if child does not exist initialize it
             let child = init(Rc::downgrade(&this));
             // if child is a widget or a container add it's widget to this if this is also a widget
-            match child.as_ref().borrow().deref() {
-                GxiNodeType::Widget(child) | GxiNodeType::Container(child) => {
-
-                }
-                _ => ()
-            }
-            if let Ok(child) = child.clone().into_gxi_widget_rc() {
+            if let Ok(child) = child.as_ref().borrow().as_widget_node() {
                 let child_borrow = child.borrow();
-                match &mut this {
+                match this_borrow_mut.deref_mut() {
                     GxiNodeType::Widget(this) => {
-                        let mut this_borrow = this.as_ref().borrow_mut();
-                        this_borrow.get_widget_mut().append(child_borrow.get_widget());
+                        this.get_widget_mut().append(child_borrow.get_widget());
                     }
                     GxiNodeType::Component(_this) => {
                         // while parent isn't widget
@@ -49,12 +42,11 @@ pub fn init_member<F: FnOnce(WeakNodeType) -> StrongNodeType>(
                             let parent = parent.into_gxi_node_rc();
                         }*/
                     }
-                    _ => unreachable!()
+                    _ => unreachable!(),
                 }
             }
 
-            let mut this_node_borrow = this_node.as_ref().borrow_mut();
-            *this_node_borrow.get_child_mut() = Some(child.clone());
+            *this_borrow_mut.as_container_mut().unwrap().get_child_mut() = Some(child.clone());
             return (child, true);
         }
         _ => {
