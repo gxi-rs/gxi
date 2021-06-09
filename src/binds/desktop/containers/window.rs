a27eae0235fefa715f230d2c69e41f159239e000
Aniket Prajapati <prajapati.ani306@gmail.com>
2021-06-09 15:41:11 +0530

    window viz

--- src/binds/desktop/containers/window.rs
+++ src/binds/desktop/containers/window.rs
@@ -10,7 +10,7 @@ pub struct Window {
     parent: WeakNodeType,
     child: Option<StrongNodeType>,
     sibling: Option<StrongNodeType>,
-    pub(crate) widget: gtk::Window,
+    widget: gtk::Window,
     self_substitute: Option<WeakNodeType>,
 }
 
