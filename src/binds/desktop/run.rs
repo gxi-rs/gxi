4166ee6a3833a95d4ccb162d83489478cad2af69
aniketfuryrocks <prajapati.ani306@gmail.com>
2021-06-11 20:02:12 +0530

    init_member

--- src/binds/desktop/run.rs
+++ src/binds/desktop/run.rs
@@ -1,12 +1,10 @@
-use crate::{init_member, InitType, Node, Root, Window};
-use std::rc::Rc;
+use crate::{init_member, InitType, Node, Root};
 
 pub fn run<App: Node + 'static>() {
     let rt = tokio::runtime::Runtime::new().unwrap();
     rt.block_on(async {
         gtk::init().unwrap();
         let root = Root::new_root();
-        let root = Window::new(Rc::downgrade(&root));
         let app = init_member(root.clone(), InitType::Child, |this| App::new(this)).0;
         App::render(app);
         gtk::main();
