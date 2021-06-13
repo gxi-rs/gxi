a47843d1e2aebdfabcadee41015e536202ce6d7c
aniketfuryrocks <prajapati.ani306@gmail.com>
2021-06-13 11:47:15 +0530

    no #children support error message

--- src/interface/get_substitute.rs
+++ src/interface/get_substitute.rs
@@ -3,11 +3,8 @@ use crate::StrongNodeType;
 pub fn get_substitute(node: StrongNodeType) -> StrongNodeType {
     let subst_borrow = node.as_ref().borrow();
     if let Ok(node_comp) = subst_borrow.as_component_node() {
-        if let Some(subst) = node_comp.get_self_substitute() {
-            return subst
-                .upgrade()
-                .expect(&format!("#children no longer exists. Make sure it lives long enough"));
-        }
+        let subst = node_comp.get_self_substitute().as_ref().expect("You are trying to add a child into component which doen't support #children");
+        return subst.upgrade().expect(&format!("#children no longer exists. Make sure it lives long enough"));
     }
     drop(subst_borrow);
     node
