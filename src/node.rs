67997053e59c3a0473928718ee74c3443920b16b
aniketfuryrocks <prajapati.ani306@gmail.com>
2021-07-20 11:31:53 +0530

    clone for nodes

--- src/node.rs
+++ src/node.rs
@@ -1,14 +1,14 @@
 use crate::StrongNodeType;
 
 /// node of a n-binary tree which can hold a child and a sibling
-#[derive(Default)]
+#[derive(Clone, Default)]
 pub struct ContainerNode {
     pub child: Option<StrongNodeType>,
     pub sibling: Option<StrongNodeType>,
 }
 
 /// node of a n-binary tree which can hold only a sibling
-#[derive(Default)]
+#[derive(Clone, Default)]
 pub struct WidgetNode {
     pub sibling: Option<StrongNodeType>,
 }
