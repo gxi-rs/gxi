e15e2848e5156ca41480ec9a6b6c1fa13fa2bc69
aniketfuryrocks <prajapati.ani306@gmail.com>
2021-09-13 16:11:43 +0530

    execution block

--- gxi-transpiler/src/execution/mod.rs
+++ gxi-transpiler/src/execution/mod.rs
@@ -1,1 +1,21 @@
-pub struct ExecutionBlock {}
+use quote::{ToTokens, TokenStreamExt, quote};
+
+pub struct ExecutionBlock(syn::Block);
+
+impl ExecutionBlock {
+    pub fn parse(input: syn::parse::ParseStream) -> syn::Result<Option<Self>> {
+        if let Ok(block) = input.parse::<syn::Block>() {
+            return Ok(Some(Self(block)));
+        }
+        Ok(None)
+    }
+}
+
+impl ToTokens for ExecutionBlock {
+    fn to_tokens(&self, tokens: &mut quote::__private::TokenStream) {
+        let stmts = &self.0.stmts;
+        tokens.append_all(quote! {
+            #(#stmts)*
+        })
+    }
+}
