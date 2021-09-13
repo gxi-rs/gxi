9366d25b16542c6377cace668464506fc2bd060b
aniketfuryrocks <prajapati.ani306@gmail.com>
2021-09-13 08:55:14 +0530

    placeholder for default feature set

--- src/platforms/default/mod.rs
+++ src/platforms/default/mod.rs
@@ -1,1 +1,16 @@
+use crate::NativeContainerExt;
 
+pub type NativeWidget = ();
+pub type NativeContainerWidget = ();
+pub type Element = ();
+
+const PANIC_MSG:&'static str ="Can't construct a tree with default features. Consider enabling other features. eg. web, desktop";
+
+impl NativeContainerExt for NativeContainerWidget {
+    fn append(&mut self, _widget: &NativeWidget) {
+        panic!("{}", PANIC_MSG);
+    }
+    fn move_to_index(&mut self, _widget: &NativeWidget, _index: usize) {
+        panic!("{}", PANIC_MSG);
+    }
+}
