27074f11e4f91ed84e669849904690a5fa31fbdd
aniketfuryrocks <prajapati.ani306@gmail.com>
2021-09-13 16:12:09 +0530

    prevent root node from dropping

--- src/platforms/web/run.rs
+++ src/platforms/web/run.rs
@@ -1,9 +1,10 @@
 use std::{cell::RefCell, rc::Rc};
 
-use crate::{Root, VNode};
+use crate::{InitType, Root, VNode, init_member};
 
 pub fn run<C: crate::VNode + crate::Renderable + 'static>() {
     let root = Rc::new(RefCell::new(Root::default().into_vnode_type()));
-    let node_rc = Rc::new(RefCell::new(C::new(Rc::downgrade(&root)).into_vnode_type()));
-    C::render(&node_rc);
+    let (node, _) = init_member(&root, InitType::Child, |parent| C::new(parent).into_vnode_type()).unwrap();
+    C::render(&node);
+    std::mem::forget(root);
 }
