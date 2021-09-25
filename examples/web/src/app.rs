397d569d849c08b59eaa9064863cad84d2b6ff3e
aniketfuryrocks <prajapati.ani306@gmail.com>
2021-09-25 17:07:30 +0530

    observable rc

--- examples/web/src/app.rs
+++ examples/web/src/app.rs
@@ -1,17 +1,18 @@
-use gxi::{gxi, Body, StrongNodeType};
-
+use gxi::{gxi, Body, StrongNodeType}; use std::ops::Deref;
+//
 fn use_state<T>(v: T) -> gxi::Observable<T> {
     gxi::Observable::new(v)
 }
-
+//
 pub fn app() -> StrongNodeType {
-    let _counter = use_state(0);
+    let mut counter = use_state(0);
     let _const_value = "yp";
-    // add this to gxi macro
+    // add this to gx
+    let mut counter2 = counter.clone();
     return gxi! {
         Body [
-           button ( /*on_click = |_| panic!("hello"),*/ inner_html = "click me"),
-           p ( inner_html = "yp" )
+           button ( on_click = move |_| counter2.set_value(*counter2 + 1), inner_html = "click me"),
+           p ( inner_html = &(counter.deref().to_string())[..] )
         ]
     };
 }
--- src/observable.rs
+++ src/observable.rs
@@ -1,26 +1,19 @@
-use std::ops::{Deref, DerefMut};
+use std::{cell::RefCell, ops::Deref, rc::Rc};
 
 pub type Observer<V> = Box<dyn FnMut(&V) -> ()>;
 
+#[derive(Clone)]
 pub struct Observable<V> {
-    observers: Vec<Observer<V>>,
-    value: V,
-    /// true if all observers have been notified about the change
-    synced: bool,
+    value: Rc<V>,
+    observers: Rc<RefCell<Vec<Observer<V>>>>,
+    value_synced: bool,
 }
 
 impl<V> Deref for Observable<V> {
     type Target = V;
 
     fn deref(&self) -> &Self::Target {
-        &self.value
-    }
-}
-
-impl<V> DerefMut for Observable<V> {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        self.synced = false;
-        &mut self.value
+        &*self.value
     }
 }
 
@@ -28,32 +21,38 @@ impl<V> Observable<V> {
     pub fn new(value: V) -> Self {
         Self {
             observers: Default::default(),
-            value,
-            synced: true,
+            value: Rc::new(value),
+            value_synced: false,
         }
     }
 
     /// mutates value, and notifies all the observers
     pub fn set_value(&mut self, value: V) {
-        **self = value;
+        self.value = Rc::new(value);
         self.notify();
     }
 
     /// add observer which is called when value under Observable changes
     pub fn add_observer(&mut self, observer: Observer<V>) {
-        self.observers.push(observer);
+        self.observers.as_ref().borrow_mut().push(observer);
     }
 
     /// notifies all observers that the value has changed
-    pub fn notify(&mut self) {
-        if !self.synced {
-            for observer in &mut self.observers {
+    pub fn notify(&self) {
+        if !self.value_synced {
+            let mut observers = self.observers.as_ref().borrow_mut();
+            for observer in &mut *observers {
                 (observer)(&self.value);
             }
         }
     }
 }
 
+impl<V> Drop for Observable<V> {
+    fn drop(&mut self) {
+        self.notify();
+    }
+}
 impl<V> From<V> for Observable<V> {
     fn from(v: V) -> Self {
         Self::new(v)
