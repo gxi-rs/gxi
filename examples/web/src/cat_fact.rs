22b58b46b3ce3550f0b0a4728fcee198da3405f0
Restyled.io <commits@restyled.io>
2021-10-25 13:16:18 +0000

    Restyled by rustfmt

--- examples/web/src/cat_fact.rs
+++ examples/web/src/cat_fact.rs
@@ -4,7 +4,7 @@ pub unsafe fn cat_fact() -> StrongNodeType {
     let cat_fact = State::new(String::new());
 
     let fetch_cat_fact = set_state!(
-        async |_| {
+        async | _ | {
             let resp = reqwest::get("https://catfact.ninja/fact?max_length=140")
                 .await
                 .unwrap();
