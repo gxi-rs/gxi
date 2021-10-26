46c3b4ac6bad46c1c14eed89c0e72b9ff402f379
aniketfuryrocks <contact@aniketprajapati.me>
2021-10-26 16:39:46 +0530

    prefetch example

--- examples/web/src/cat_fact.rs
+++ examples/web/src/cat_fact.rs
@@ -4,7 +4,7 @@ pub fn cat_fact() -> StrongNodeType {
     let cat_fact = State::new(String::new());
 
     let fetch_cat_fact = set_state!(
-        async |_| {
+        async || {
             let resp = reqwest::get("https://catfact.ninja/fact?max_length=140")
                 .await
                 .unwrap();
@@ -13,9 +13,11 @@ pub fn cat_fact() -> StrongNodeType {
         [cat_fact]
     );
 
+    fetch_cat_fact();
+
     return gxi! {
         div [
-            button ( const on_click = fetch_cat_fact ) [
+            button ( on_click = move |_| fetch_cat_fact() ) [
                 Text ( value = "fetch cat memes!" )
             ],
             p [
