429a61cd5e527964f64e78f92f9cd95a6259b67b
aniketfuryrocks <contact@aniketprajapati.me>
2021-10-30 10:41:01 +0530

    todo_comp

--- examples/web/src/app.rs
+++ examples/web/src/app.rs
@@ -1,11 +1,12 @@
-use crate::{cat_fact, complex_counter};
+use crate::{cat_fact, complex_counter, todo};
 use gxi::{gxi, Body, StrongNodeType};
 
 pub fn app() -> StrongNodeType {
     return gxi! {
         Body [
             complex_counter(),
-            cat_fact()
+            cat_fact(),
+            todo()
         ]
     };
 }
--- examples/web/src/todo.rs
+++ examples/web/src/todo.rs
@@ -1,20 +1,20 @@
-use gxi::{gxi, State, StrongNodeType, Text, set_state};
+use gxi::{gxi, set_state, State, StrongNodeType};
 
 pub fn todo() -> StrongNodeType {
     let todos = State::new(String::new());
-   
+
     return gxi! {
-        div [
-            input ( on_input = set_state!(|e| {
-               let data = e.data().unwrap_or(String::new());
-               todos.push_str(&data);
-            }, [ref todos] ) ),
-            for x in todos {
-                    span [
-                        Text ( value = &todos[..] )
-                    ]
-                }
-            }
-        ]
-    };
+            div [
+                input ( on_input = set_state!(|e| {
+                   let data = e.data().unwrap_or(String::new());
+                   todos.push_str(&data);
+                }, [ref todos] ) ),
+     //           for x in todos {
+     //                   span [
+     //                       Text ( value = &todos[..] )
+     //                   ]
+     //               }
+    //            }
+            ]
+        };
 }
