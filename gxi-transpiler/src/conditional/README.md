8a4713d88a7cf82b0a1ff627b696e09d24e9b8fa
aniketfuryrocks <contact@aniketprajapati.me>
2021-10-31 14:08:08 +0530

    readme

---
+++ gxi-transpiler/src/conditional/README.md
@@ -0,0 +1,24 @@
+## Conditional Block
+
+### Syntax
+
+_if expr_
+
+```rust
+if <keyword> expr {
+
+} ...arms
+```
+
+_match expr_
+
+```rust
+match <keyword> expr {
+  ...arms
+}
+```
+
+**optional `<keyword>`**
+
+- `const`
+   marks expression as constant i.e not observable.
--- gxi-transpiler/src/conditional/block.rs
+++ gxi-transpiler/src/conditional/block.rs
@@ -1,36 +1,4 @@
-use quote::ToTokens;
-use syn::Token;
+#[doc(include_str="./README.md")]
+pub struct ConditionalBlock {
 
-pub struct ConditionalBlock {}
-
-impl ConditionalBlock {
-    fn parse(input: syn::parse::ParseStream) -> syn::Result<Option<Self>> {
-        if input.peek(Token!(if)) {
-            let syn::ExprIf {
-                if_token,
-                cond,
-                then_branch,
-                else_branch,
-                ..
-            } = input.parse::<syn::ExprIf>()?;
-        } else if input.peek(Token!(match)) {
-            let syn::ExprMatch {
-                match_token,
-                expr,
-                brace_token,
-                arms,
-                ..
-            } = input.parse::<syn::ExprMatch>()?;
-        } else {
-            return Ok(None);
-        }
-
-        Ok(Some(Self {}))
-    }
-}
-
-impl ToTokens for ConditionalBlock {
-    fn to_tokens(&self, tokens: &mut quote::__private::TokenStream) {
-        todo!()
-    }
 }
