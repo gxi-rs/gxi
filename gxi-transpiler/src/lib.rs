485883ea0e16124f7088311302f8506ec3423e71
aniketfuryrocks <contact@aniketprajapati.me>
2021-12-24 22:30:41 +0530

    comment

--- gxi-transpiler/src/lib.rs
+++ gxi-transpiler/src/lib.rs
@@ -16,17 +16,9 @@ mod optional_parse;
 #[doc = include_str!("../README.md")]
 #[proc_macro]
 pub fn gxi(input: TokenStream) -> TokenStream {
+    // Other blocks can't exist independently
     let block = syn::parse_macro_input!(input as NodeBlock);
     let mut tokens = TokenStream2::new();
     block.to_tokens(&mut tokens);
-    //TODO: support this
-    //block.to_tokens(
-    //    &mut tokens,
-    //    0,
-    //    &quote! {
-    //        compile_error!("independent blocks other than node are not supported yet");
-    //    }
-    //);
-    //    println!("\n{}\n", tokens.to_string());
     tokens.into()
 }
